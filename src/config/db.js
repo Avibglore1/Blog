5a6094ed1c012365330f8e022dab5a9d8f853a13
Avinash <avinashkumarofficial95@gmail.com>
2026-02-01 21:29:33 +0530

    feat: add post creation and update functionality with multer for file uploads

--- src/config/db.js
+++ src/config/db.js
@@ -5,7 +5,7 @@ export const connectDB = async () =>{
         await mongoose.connect(process.env.MONGO_URI);
         console.log('MongoDB Connected')
     } catch (error) {
-    console.error(err);
+    console.error(error);
     process.exit(1);  
     }
 }
---
+++ src/controllers/post.controller.js
@@ -0,0 +1,31 @@
+import Post from "./../models/Post.js"
+
+export const createPost = async (req,res) =>{
+    try {
+        const post = await Post.create({
+        title: req.body.title,
+        body: req.body.body,
+        image: req.file?.path,
+        author: req.user.id
+    });
+    res.status(201).json(post);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({message: "Server Error"})
+    } 
+}
+
+export const updatePost = async (req,res) =>{
+    try {
+        const post = await Post.findById(req.params.id);
+        if(!post) return res.status(404).json({message: "Post not found"});
+        if(post.author.toString()!==req.user.id)
+            return res.status(403).json({message: "Not allowed"});
+        post.title = req.body.title || post.title;
+        post.body = req.body.body || post.body;
+        if(req.file) post.image = req.file.path;
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({message: "Server Error"})
+    }
+}
