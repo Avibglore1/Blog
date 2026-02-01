a698f4c5ea21d327cd2280d66ce04d47625ca764
Avinash <avinashkumarofficial95@gmail.com>
2026-02-01 23:08:09 +0530

    feat: add authentication and image upload middleware to post routes

--- src/routes/post.routes.js
+++ src/routes/post.routes.js
@@ -1,9 +1,11 @@
 import { createPost, updatePost } from "../controllers/post.controller.js";
 import express from "express";
+import { protect } from "../middleware/auth.middleware.js";
+import { upload } from "../middleware/upload.middleware.js";
 
 const router = express.Router();
 
-router.post("/", createPost);
-router.post("/:id", updatePost);
+router.post("/",protect,upload.single("image"), createPost);
+router.post("/:id",protect,upload.single("image"), updatePost);
 
 export default router
